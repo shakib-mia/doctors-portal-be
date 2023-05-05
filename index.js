7c0830f0448b3f29325fd1e51553417431c73228
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2023-05-05 19:45:20 +0600

    db connected

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 // import express from 'express'
 const jwt = require('jsonwebtoken')
 require('dotenv').config();
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const app = express();
 
@@ -10,6 +11,9 @@ app.use(cors())
 app.use(express.json())
 
 const port = process.env.PORT || 5000;
+const uri = `mongodb+srv://doctor_admin:${process.env.db_pass}@cluster0.qfivt.mongodb.net/?retryWrites=true&w=majority`
+
+// console.log(process.env.db_pass);
 
 app.get("/", (req, res) => {
     res.send('Welcome To Doctors Portal Server');
@@ -31,5 +35,37 @@ app.get("/user", (req, res) => {
     const user = jwt.decode(token)
     res.send(user)
 })
+
+
+const verifyJWT = (req, res, send) => {
+    const authHeader = req.headers.token;
+    // console.log(authHeader);
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized' })
+    }
+
+    send();
+}
+
+
+const client = new MongoClient(uri, { serverApi: { version: ServerApiVersion.v1, strict: true, deprecationErrors: true, } });
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const collection = client.db('doctors_portal').collection('booking')
+
+        app.get('/appointments', verifyJWT, async (req, res) => {
+            const query = {};
+            const cursor = collection.find(query)
+            const bookings = await cursor.toArray()
+            res.send(bookings)
+        })
+
+    } finally { }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => console.log(`listening on port: ${port}`))
